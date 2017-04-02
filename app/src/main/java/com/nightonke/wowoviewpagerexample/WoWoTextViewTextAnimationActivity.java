500ef550b45f78a8aefb9439281b7307c2e90cd6
Nightonke <Nightonke@outlook.com>
2017-04-02 18:45:11 +0800

    1. More typewriters.

--- app/src/main/java/com/nightonke/wowoviewpagerexample/WoWoTextViewTextAnimationActivity.java
+++ app/src/main/java/com/nightonke/wowoviewpagerexample/WoWoTextViewTextAnimationActivity.java
@@ -1,9 +1,10 @@
 package com.nightonke.wowoviewpagerexample;
 
 import android.os.Bundle;
+import android.view.View;
 
-import com.nightonke.wowoviewpager.Animation.ViewAnimation;
 import com.nightonke.wowoviewpager.Animation.WoWoTextViewTextAnimation;
+import com.nightonke.wowoviewpager.Enum.Typewriter;
 
 public class WoWoTextViewTextAnimationActivity extends WoWoActivity {
 
@@ -16,16 +17,26 @@ public class WoWoTextViewTextAnimationActivity extends WoWoActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ViewAnimation animation = new ViewAnimation(findViewById(R.id.test));
-        animation.add(WoWoTextViewTextAnimation.builder().page(0).start(0).end(1).from("Nightonke").to("WoWoViewPager").build());
-        animation.add(WoWoTextViewTextAnimation.builder().page(1).start(0).end(1).from("WoWoViewPager").to("").build());
-        animation.add(WoWoTextViewTextAnimation.builder().page(2).start(0).end(1).from("").to("Nightonke").build());
-        animation.add(WoWoTextViewTextAnimation.builder().page(3).start(0).end(0.5).from("Nightonke").to("Huang").build());
-        animation.add(WoWoTextViewTextAnimation.builder().page(3).start(0.5).end(1).from("Huang").to("Weiping").build());
-        wowo.addAnimation(animation);
+        addTextAnimation(findViewById(R.id.test0), Typewriter.InsertFromLeft);
+        addTextAnimation(findViewById(R.id.test1), Typewriter.DeleteThenType);
+        addTextAnimation(findViewById(R.id.test2), Typewriter.InsertFromRight);
 
         wowo.setEase(ease);
         wowo.setUseSameEaseBack(useSameEaseTypeBack);
         wowo.ready();
+    }
+
+    private void addTextAnimation(View view, Typewriter typewriter) {
+        wowo.addAnimation(view)
+                .add(WoWoTextViewTextAnimation.builder().page(0).start(0).end(1)
+                        .from("Nightonke").to("WoWoViewPager").typewriter(typewriter).build())
+                .add(WoWoTextViewTextAnimation.builder().page(1).start(0).end(1)
+                        .from("WoWoViewPager").to("").typewriter(typewriter).build())
+                .add(WoWoTextViewTextAnimation.builder().page(2).start(0).end(1)
+                        .from("").to("Nightonke").typewriter(typewriter).build())
+                .add(WoWoTextViewTextAnimation.builder().page(3).start(0).end(0.5)
+                        .from("Nightonke").to("Huang").typewriter(typewriter).build())
+                .add(WoWoTextViewTextAnimation.builder().page(3).start(0.5).end(1)
+                        .from("Huang").to("Weiping").typewriter(typewriter).build());
     }
 }
--- wowoviewpager/src/main/java/com/nightonke/wowoviewpager/Enum/Typewriter.java
+++ wowoviewpager/src/main/java/com/nightonke/wowoviewpager/Enum/Typewriter.java
@@ -9,27 +9,56 @@ package com.nightonke.wowoviewpager.Enum;
 
 public enum Typewriter {
 
-    DeleteThenType;
+    DeleteThenType,
+    InsertFromLeft,
+    InsertFromRight;
 
     public static String nextString(String fromText, String toText, float offset, Typewriter typewriter) {
         if (offset < 0) offset = 0;
         if (offset > 1) offset = 1;
         switch (typewriter) {
-            case DeleteThenType:
-            {
-                int sum = (fromText.length() + toText.length()) * 2;
-                float current = sum * offset;
-                if (current < 1) return fromText;
-                else if (current < sum - toText.length() * 2 - 1) {
-                    return fromText.substring(0, fromText.length() - (int) ((current + 1) / 2));
-                } else if (current < fromText.length() * 2 + 1) {
-                    return "";
-                } else if (current < sum - 1) {
-                    return toText.substring(0, (int) ((current - fromText.length() * 2 + 1) / 2));
-                } else return toText;
-            }
+            case DeleteThenType: return deleteThenType(fromText, toText, offset);
+            case InsertFromLeft: return insertFromLeft(fromText, toText, offset);
+            case InsertFromRight: return insertFromRight(fromText, toText, offset);
         }
         return nextString(fromText, toText, offset, DeleteThenType);
+    }
+
+    private static String deleteThenType(String fromText, String toText, float offset) {
+        int sum = (fromText.length() + toText.length()) * 2;
+        float current = sum * offset;
+        if (current < 1) return fromText;
+        else if (current < sum - toText.length() * 2 - 1) {
+            return fromText.substring(0, fromText.length() - (int) ((current + 1) / 2));
+        } else if (current < fromText.length() * 2 + 1) {
+            return "";
+        } else if (current < sum - 1) {
+            return toText.substring(0, (int) ((current - fromText.length() * 2 + 1) / 2));
+        } else return toText;
+    }
+
+    private static String insertFromLeft(String fromText, String toText, float offset) {
+        int sum = Math.max(fromText.length(), toText.length()) * 2;
+        float current = sum * offset;
+        if (current < 1) return fromText;
+        else if (current < sum - 1) {
+            int length = (int) ((sum - current + 1) / 2);
+            String pre = toText.substring(0, Math.min((int) ((current + 1) / 2), toText.length()));
+            String post = fromText.substring(0, Math.min(length, fromText.length()));
+            return pre + post;
+        } else return toText;
+    }
+
+    private static String insertFromRight(String fromText, String toText, float offset) {
+        int sum = Math.max(fromText.length(), toText.length()) * 2;
+        float current = sum * offset;
+        if (current < 1) return fromText;
+        else if (current < sum - 1) {
+            int length = (int) ((sum - current + 1) / 2);
+            String pre = toText.substring(0, Math.min((int) ((current + 1) / 2), toText.length()));
+            String post = fromText.substring(0, Math.min(length, fromText.length()));
+            return post + pre;
+        } else return toText;
     }
 
 }
